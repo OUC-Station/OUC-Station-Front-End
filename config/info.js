9167ce654c084c3da79e0f8c1dcb2e7955243f23
WYB85 <476507763@qq.com>
2021-05-22 17:47:37 +0800

    feat: 已完成地图打点

---
+++ config/info.js
@@ -0,0 +1,10 @@
+//这里用来存储路径
+const path = 
+[
+    
+]
+
+
+module.exports = {
+    path
+}
--- pages/map/map.js
+++ pages/map/map.js
@@ -1,66 +1,105 @@
-// pages/map/map.js
+const info = require('../../config/info.js')
+const site = require('../../config/site.js')
+var app = getApp();
+var url = app.globalData.urlPath;
+
 Page({
-
-  /**
-   * 页面的初始数据
-   */
   data: {
-
+    latitude: 36.16381384970564,  //默认定位经度
+    longitude: 120.49716213967224, //默认定位维度
+    polyline: [{
+      points: info.path,
+      color: "#a885d5",
+      width: 5,
+      borderColor: '#414f3e',
+      borderWidth: 1
+    }],
+    markers: site.site
+  },
+  
+  points: [
+  ],
+  recording: false,
+  onReady: function (e) {
+    this.mapCtx = wx.createMapContext('myMap')
+  },
+  getCenterLocation: function () {
+    this.mapCtx.getCenterLocation({
+      success: function(res){
+        console.log(res.longitude)
+        console.log(res.latitude)
+      }
+    })
+  },
+  drawPath: function() {
+    let newpath = this.points
+    if (newpath.length < 2) return
+    this.setData({
+      polyline: [{
+        points: newpath,
+        color: "#a885d5",
+        width: 5,
+        borderColor: '#414f3e',
+        borderWidth: 1
+      }]
+    })
+  },
+  bindmaptap: function (e) {
+    if (!this.recording) return
+    let point = {
+      latitude: e.detail.latitude,
+      longitude: e.detail.longitude
+    }
+    this.points.push(point)
+    console.log(JSON.stringify(point))
+    this.drawPath()
+  },
+  startRecord: function (e) {
+    this.recording = true;
+  },
+  printPath: function (e) {
+    console.log(JSON.stringify(this.points))
+  },
+  pauseRecord: function (e) {
+    this.recording = false;
+  },
+  stopRecord: function (e) {
+    this.recording = false;
+    console.log("将以下内容复制到config/info.path即可更新路径")
+    this.printPath();
+    this.points = []
+    console.log("录制结束，路径清空！")
+  },
+  goback: function() {
+    this.points.pop()
+    this.drawPath()
   },
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
+
+
+
   onLoad: function (options) {
-
+    /*
+    wx.request({
+          url: url+'/api/location/get_locations_list',
+          method: "GET",
+          header: {
+           "Content-Type": "application/x-www-form-urlencoded"   
+           },
+         data: {
+           
+         },
+         success: function (res) {
+             console.log("地图：",res)
+              
+        },
+       })
+     */
   },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+  bindmarkertap: function(e){//跳转到地图详情
+    console.log("此时点击了：",e.detail)
+    wx.navigateTo({
+      url: '/pages/mapdetail/mapdetail',
+    })
   }
 })
