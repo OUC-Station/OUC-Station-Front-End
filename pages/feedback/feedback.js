3d09b5ea4449c414fee2f54b873d306fef308d24
WYB85 <476507763@qq.com>
2021-06-10 16:56:40 +0800

    feat: 反馈界面已完成

--- pages/feedback/feedback.js
+++ pages/feedback/feedback.js
@@ -9,15 +9,149 @@ Page({
    * 页面的初始数据
    */
   data: {
+    windowHeight: app.globalData.windowHeight, //手机高度
+    phone: '',
+    sentTag:'',
+    sentText:'',
+    lists:[
+      {id:0, tagColor: '#535b72', bgColor: '#f3f6ff', tagtext:'功能异常'},
+      {id:1, tagColor: '#535b72', bgColor: '#f3f6ff', tagtext:'产品建议'},
+      {id:2, tagColor: '#535b72', bgColor: '#f3f6ff', tagtext:'我要吐槽'},
+      {id:3, tagColor: '#535b72', bgColor: '#f3f6ff', tagtext:'其他问题'}
+    ], 
+    count1:0,
+    count2:0,
+    count3:0,
+    count4:0,
    //字数限制
-   maxWord: 500,
+   maxWord: 300,
    currentWord: 0,
+   isSent: true // true代表不能发送
   },
-
+  onChange(event) {
+    this.setData({
+       phone: '【'+event.detail+'】'
+     })
+  },
+  toTag: function(e){
+     var newlists = this.data.lists;
+     let id = e.currentTarget.dataset.id;
+     console.log("id",id);
+     for(var i=0; i<4; i++){
+      newlists[i].bgColor =  '#f3f6ff';
+      newlists[i].tagColor = '#535b72';
+    }
+     if(id == 0){
+      this.setData({
+        count2:0,
+        count3:0,
+        count4:0,
+       })
+        if(this.data.count1++%2 == 0){
+          newlists[id].bgColor =  '#e5faff';
+          newlists[id].tagColor = '#00cafc';
+          this.setData({
+            lists: newlists,
+            sentTag:'【功能异常】'
+          })
+        }else{
+          newlists[id].bgColor =  '#f3f6ff';
+          newlists[id].tagColor = '#535b72';
+          this.setData({
+            lists: newlists,
+            sentTag:''
+          })
+        }
+     }else if(id == 1){
+      this.setData({
+        count1:0,
+        count3:0,
+        count4:0,
+       })
+      if(this.data.count2++%2 == 0){
+        newlists[id].bgColor =  '#e5faff';
+        newlists[id].tagColor = '#00cafc';
+        this.setData({
+          lists: newlists,
+          sentTag:'【产品建议】'
+        })
+      }else{
+        newlists[id].bgColor =  '#f3f6ff';
+        newlists[id].tagColor = '#535b72';
+        this.setData({
+          lists: newlists,
+          sentTag:''
+        })
+      }
+     }else if(id == 2){
+      this.setData({
+        count2:0,
+        count1:0,
+        count4:0,
+       })
+      if(this.data.count3++%2 == 0){
+        newlists[id].bgColor =  '#e5faff';
+        newlists[id].tagColor = '#00cafc';
+        this.setData({
+          lists: newlists,
+          sentTag:'【我要吐槽】'
+        })
+      }else{
+        newlists[id].bgColor =  '#f3f6ff';
+        newlists[id].tagColor = '#535b72';
+        this.setData({
+          lists: newlists,
+          sentTag:''
+        })
+      }
+     }else{
+      this.setData({
+        count2:0,
+        count3:0,
+        count1:0,
+       })
+      if(this.data.count4++%2 == 0){
+        newlists[id].bgColor =  '#e5faff';
+        newlists[id].tagColor = '#00cafc';
+        this.setData({
+          lists: newlists,
+          sentTag:'【其他问题】'
+        })
+      }else{
+        newlists[id].bgColor =  '#f3f6ff';
+        newlists[id].tagColor = '#535b72';
+        this.setData({
+          lists: newlists,
+          sentTag:''
+        })
+      }
+     }
+     console.log("---------------",this.data.sentTag);
+     if(this.data.currentWord && this.data.sentTag){
+      this.setData({
+         isSent: false
+      })
+    }else{
+     this.setData({
+       isSent: true
+    })
+    }
+  },
   limitWord: function (e) {//字数限制
+    if(e.detail.value && this.data.sentTag){
+      this.setData({
+         isSent: false
+      })
+    }else{
+     this.setData({
+       isSent: true
+    })
+    }
     var that = this;
     var value = e.detail.value;
-    content = e.detail.value;
+    that.setData({
+      sentText:e.detail.value
+    })
     var wordLength = parseInt(value.length); //解析字符串长度转换成整数。
     if (that.data.maxWord < wordLength) {
       return;
@@ -27,6 +161,7 @@ Page({
     });
   },
   submit: function(){
+    content = this.data.sentTag + this.data.phone + this.data.sentText;
     console.log("-------------------------------",content)
     Dialog.confirm({
       message: '确定提交',
