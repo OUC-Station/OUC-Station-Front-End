ea82b8f36ddbbd3b0701cd54596aa48eb34d6b07
WYB85 <476507763@qq.com>
2021-06-10 18:17:43 +0800

    feat: 活动中心详情添加回到顶部功能，同时更新活动列表数据

--- pages/activitydetail/activitydetail.js
+++ pages/activitydetail/activitydetail.js
@@ -4,7 +4,7 @@ moment.locale('zh-cn');
 const app = getApp();
 var url = app.globalData.urlPath;
 var top = Math.round(app.globalData.top*0.85); //胶囊按钮到顶部的距离乘以0.85再四舍五入
-var windowWidth = Math.round(app.globalData.windowWidth*0.85);// 手机屏幕宽度乘以0.85再四舍五入
+var windowWidth = Math.round(app.globalData.windowWidth*0.89);// 手机屏幕宽度乘以0.89再四舍五入
 Page({
 
   /**
@@ -43,7 +43,6 @@ Page({
              });
             //处理图片样式
             listall.content = listall.content.replace(/\<img/g, '<img style="width:' + windowWidth + 'px;height:auto;"');
-
               console.log("活动列表数据详情：",listall);
               that.setData({
                  cover: listall.cover,
@@ -58,52 +57,31 @@ Page({
   naviback: function(){
      wx.navigateBack({})
   },
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+  
+  // 获取滚动条当前位置
+ onPageScroll: function (e) {
+  console.log(e)
+  if (e.scrollTop > 100) {
+    this.setData({
+      floorstatus: true
+    });
+  } else {
+    this.setData({
+      floorstatus: false
+    });
   }
+},
+  //回到顶部
+goTop: function (e) {  // 一键回到顶部
+  if (wx.pageScrollTo) {
+    wx.pageScrollTo({
+      scrollTop: 0
+    })
+  } else {
+    wx.showModal({
+      title: '提示',
+      content: '当前微信版本过低，无法使用该功能，请升级到最新微信版本后重试。'
+    })
+  }
+}
 })
